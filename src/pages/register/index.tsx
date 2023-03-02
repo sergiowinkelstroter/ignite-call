2e9e52c766cb76c74931821321b5ca4356232ba6
sergio <winksousa0@gmail.com>
2023-03-02 20:42:05 -0300

    connect on calendar

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -6,6 +6,8 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { api } from "@/lib/axios";
+import { AxiosError } from "axios";
 
 const registerFormSchema = z.object({
   username: z
@@ -41,7 +43,21 @@ export default function Register() {
   }, [router.query?.username, setValue]);
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data);
+    try {
+      await api.post("/users", {
+        name: data.name,
+        username: data.username,
+      });
+
+      await router.push("/register/connect-calendar");
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message);
+        return;
+      }
+
+      console.error(err);
+    }
   }
 
   return (
